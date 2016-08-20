a66ce536f9942288563513f1fbf4d8ce5033e39a
Hung Dao Luong <hungdl@imt-soft.com>
2016-08-20 09:21:15 +0700

    no message

---
+++ GoBuddies/GoBuddies.Server.Data/DbModels/ApplicationUser.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoBuddies.Server.Data.DbModels
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [MaxLength(100)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string LastName { get; set; }
+
+        [Required]
+        public byte Level { get; set; }
+
+        [Required]
+        public DateTime JoinDate { get; set; }
+    }
+}
