347696cfbcc9e89e475683d290d0721baba8de7b
Hung Dao Luong <hungdl@imt-soft.com>
2016-08-20 18:22:40 +0700

    setup server

---
+++ GoBuddies/GoBuddies.Server.WebApi/Program.cs
@@ -0,0 +1,23 @@
+﻿using GoBuddies.Server.WebApi;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoBuddies.Server.WebApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                          .UseKestrel()
+                          .UseStartup<Startup>()
+                          .Build();
+
+            host.Run();
+        }
+    }
+}
